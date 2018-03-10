557d383856d9c4d2556e2e2b17e3ef4385e02fbb
许运昌 <xuyunchang@mzjmedia.net>
2018-03-10 12:46:13 +0800

    launchAtivity

--- app/src/main/java/com/xyc/musicplay/UI/LaunchActivity.java
+++ app/src/main/java/com/xyc/musicplay/UI/LaunchActivity.java
@@ -1,15 +1,21 @@
 package com.xyc.musicplay.UI;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.xyc.guguviews.views.BaseActivity;
 import com.xyc.musicplay.R;
 
-public class LaunchActivity extends AppCompatActivity {
+public class LaunchActivity extends BaseActivity {
+
+    @Override
+    protected void initHeader() {
+      setHeaderVisibility(View.GONE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launch);
+        setCenterView(R.layout.activity_launch);
     }
 }
